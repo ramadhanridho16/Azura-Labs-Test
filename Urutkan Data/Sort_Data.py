f8033abaf50ef19d40e8b124b4bc5801181cb2de
Ramadhan Ridho <ramadhan.ridho.16@students.unnes.ac.id>
2022-07-26 10:12:39 +0700

    Add variable

---
+++ Urutkan Data/Sort_Data.py
@@ -0,0 +1,14 @@
+'''
+Urutkan array di atas dengan prioritas sebagai berikut
+harga terendah
+jika harga sama maka urutkan berdasarkan rating tertinggi
+jika rating sama maka urutkan berdasarkan likes terbanyak. 
+Pastikan program yang anda buat dapat menghandle sekitar ratusan data.
+'''
+
+Indomie = [3000, 5, 150]
+Laptop = [4000000, 4.5, 123]
+Aqua = [3000, 4, 250]
+Smart_TV = [4000000, 4.5, 42]
+Headphone = [4000000, 3.5, 90]
+Very_Smart_TV = [4000000, 3.5, 87]
