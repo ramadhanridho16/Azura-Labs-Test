7d54017634941ff9c538cb3d60075e0e727fcd56
Ramadhan Ridho <ramadhan.ridho.16@students.unnes.ac.id>
2022-07-27 10:58:28 +0700

    add code for calculator

--- Kalkulator Sederhana/calculator.py
+++ Kalkulator Sederhana/calculator.py
@@ -1,4 +1,29 @@
-cal = input("Masukkan Hitungan anda: ")
+choice = input('''
+Silakan pilih jenis operasi yang ingin Anda lakukan:
++ untuk penambahan
+- untuk pengurangan
+* untuk perkalian
+/ untuk pembagian
+''')
 
+num_1 = int(input('Masukkan angka pertama: '))
+num_2 = int(input('Masukkan angka kedua: '))
 
-print(cal)
+if choice == '+':
+    print('{} + {} = '.format(num_1, num_2))
+    print(num_1 + num_2)
+
+elif choice == '-':
+    print('{} - {} = '.format(num_1, num_2))
+    print(num_1 - num_2)
+
+elif choice == '*':
+    print('{} * {} = '.format(num_1, num_2))
+    print(num_1 * num_2)
+
+elif choice == '/':
+    print('{} / {} = '.format(num_1, num_2))
+    print(num_1 / num_2)
+
+else:
+    print('Masukkan operator yang valid, jalankan kembali programnya.')
